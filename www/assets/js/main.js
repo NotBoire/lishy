17f9b13b7e29a8fdc666b07ee77d26189fc7aed9
Antoine Boire <antoine.boire@radiofrance.com>
2018-01-04 15:18:03 +0100

    Update burger

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,11 +1,11 @@
-$('.burger img').click(function() {
-  var src = ($(this).attr('src') === 'assets/img/menu.png')
-    ? 'assets/img/cancel.png'
-    : 'assets/img/menu.png';
-  $(this).attr('src', src);
+$(document).ready(function(){
+  $('.circle, .burger').click(function() {
+    $('.burger').toggleClass('open');
 
-  var header = $('.header');
-  header.hasClass('openmenu')
-    ? header.removeClass('openmenu')
-    : header.addClass('openmenu');
+    var header = $('.header');
+    header.hasClass('openmenu')
+      ? header.removeClass('openmenu')
+      : header.addClass('openmenu');
+  });
 });
+
