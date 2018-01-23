aa2cf9a909911e4ebaa47cedfc66e5d64849f910
Antoine Boire <antoine.boire@radiofrance.com>
2018-01-23 11:42:52 +0100

    Try to improve for Safari

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -2,8 +2,15 @@ $(document).ready(function(){
   $('.circle, .burger').click(function() {
     $('.burger').toggleClass('open');
     $('.navLink').toggleClass('slide');
-
-    
+    if ($('.openmenu').hasClass('slide')) {
+      setTimeout(function() {
+        $(".openmenu").removeClass('slide');
+        $(".header").removeClass('slide');
+      }, 800);
+    } else {
+      $( ".openmenu" ).addClass('slide');
+      $( ".header" ).addClass('slide');
+    }
   });
 
 });
