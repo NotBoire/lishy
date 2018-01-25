01bd6be817810bc0ef6b1c935280f59a24c01607
Antoine Boire <antoine.boire@radiofrance.com>
2018-01-25 11:50:42 +0100

    Factorize CSS - rework project handler

--- www/ContentHandler.php
+++ www/ContentHandler.php
@@ -3,7 +3,7 @@
 class ContentHandler {
     function get($project) {
         $twigLoader = new TwigLoader();
-        $template = 'project.html.twig';
+        $template = $project.'.html.twig';
         $params = array('project' => $project);
         $twigLoader->load(dirname(__FILE__) . '/views', $template, $params);
     }
