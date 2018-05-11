fcc19db98424877101163782818116701b0f67f5
Antoine Boire <antoine.boire@radiofrance.com>
2018-05-11 18:35:18 +0200

    Update menu on desk. WIP

--- www/assets/js/main.js
+++ www/assets/js/main.js
@@ -1,17 +1,25 @@
 $(document).ready(function(){
   $('.circle, .burger').click(function() {
+
     $('.burger').toggleClass('open');
     $('.navLink').toggleClass('slide');
+
+
     if ($('.openmenu').hasClass('slide')) {
+      $("#tile-blurb").css("opacity", 0.81);
       setTimeout(function() {
         $(".openmenu").removeClass('slide');
         $(".header").removeClass('slide');
-      }, 800);
+      }, 600);
+      $(".openmenu").addClass('slide-desk');
+      $(".header").addClass('slide-desk');
     } else {
+      $("#tile-blurb").css("opacity", 1);
       $( ".openmenu" ).addClass('slide');
       $( ".header" ).addClass('slide');
+      $(".openmenu").removeClass('slide-desk');
+      $(".header").removeClass('slide-desk');
     }
   });
 
 });
-
